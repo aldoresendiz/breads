0e5cacfe6b9dc711e26d97d6da24ad831b67f5c2
aldoresendiz <aldoresendiz@gmail.com>
2023-10-26 19:19:35 -0500

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -13,22 +13,35 @@ breads.get('/', (req, res) =>
     //res.send(Bread[req.params.arrayIndex]);
 });
 
-//SHOW
+// SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(`
-        <html>
-            <head>
-                <title>My bread!</title>
-            </head>
-            <body>
-                <p>Yes - I have ${Bread[req.params.arrayIndex].name}</p>
-                <img src="${Bread[req.params.arrayIndex].image}" style="width:200;height:200;">
-                <p>Try the recipe <a href="${Bread[req.params.arrayIndex].recipe}">here!</a></p>
-                <p><a href=".">Back to Menu</a></p>
-            </body>
-        </html>`
-    );
-   // res.send("Yes - I have " +  Bread[req.params.arrayIndex].name)
-})
+    if (Bread[req.params.arrayIndex]) {
+        res.render('Show', {
+            bread: Bread[req.params.arrayIndex]
+          })    
+    } else {
+        res.send('404');
+    }
+    
+  })
+  
+  
+// SHOW
+// breads.get('/:arrayIndex', (req, res) => {
+//     res.send(`
+//         <html>
+//             <head>
+//                 <title>My bread!</title>
+//             </head>
+//             <body>
+//                 <p>Yes - I have ${Bread[req.params.arrayIndex].name}</p>
+//                 <img src="${Bread[req.params.arrayIndex].image}" style="width:200;height:200;">
+//                 <p>Try the recipe <a href="${Bread[req.params.arrayIndex].recipe}">here!</a></p>
+//                 <p><a href=".">Back to Menu</a></p>
+//             </body>
+//         </html>`
+//     );
+//    // res.send("Yes - I have " +  Bread[req.params.arrayIndex].name)
+// })
 
 module.exports = breads;
