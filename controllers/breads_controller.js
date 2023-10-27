187c58b253a29ebdee1926f14918225559e0fc9b
aldoresendiz <aldoresendiz@gmail.com>
2023-10-27 00:13:44 -0500

    create and new routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -13,6 +13,11 @@ breads.get('/', (req, res) =>
     //res.send(Bread[req.params.arrayIndex]);
 });
 
+// NEW
+breads.get('/new', (req, res) => {
+    res.render('new');
+});
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
@@ -20,11 +25,25 @@ breads.get('/:arrayIndex', (req, res) => {
             bread: Bread[req.params.arrayIndex]
           })    
     } else {
-        res.send('404');
+        res.send('404')
     }
-    
   })
   
+  //CREATE
+  breads.post('/', (req, res) => {
+    if (!req.body.image) {
+        req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+    };
+    console.log(req.body);
+    if (req.body.hasGluten === 'on') {
+        req.body.hasGluten = 'true'
+    } else {
+        req.body.hasGluten = 'false'
+    }
+    Bread.push(req.body);
+    res.redirect('/breads');
+    res.send(Bread);
+  })
   
 // SHOW
 // breads.get('/:arrayIndex', (req, res) => {
